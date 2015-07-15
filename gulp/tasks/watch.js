05ebec6cd911974612d18a8a573631eeccb73a2b
Bob Yexley <bob@yexley.net>
2015-07-15 09:42:36 -0400

    Gulp watch task working

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,14 @@
+var path = require("path");
+var gulp = require("gulp");
+var buildConfig = require("../config");
+
+gulp.task("watch", function () {
+
+  gulp.watch(path.resolve(buildConfig.sourceRoot, "css/**/*.css"), ["css"]);
+  gulp.watch(path.resolve(buildConfig.sourceRoot, "js/**/*.js"), ["webpack"]);
+  gulp.watch([
+    path.resolve(buildConfig.sourceRoot, "index.html"),
+    path.resolve(buildConfig.sourceRoot, "index.js")
+  ], ["copy:static"]);
+
+});
